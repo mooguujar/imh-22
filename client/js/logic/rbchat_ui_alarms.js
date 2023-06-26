ad78804b0d0d51557b00ce6601b336e37a82ff42
sky <liyi99891100@gmail.com>
2023-06-26 15:44:03 +0800

    #去掉logo日志

--- client/js/logic/rbchat_ui_alarms.js
+++ client/js/logic/rbchat_ui_alarms.js
@@ -1339,7 +1339,6 @@ var RBChatAlarmsUI = (function () {
             unReadNum : cnt > 0 ? cnt:0,
             from:'chat'
         }
-        console.log('发送message-',obj)
         // 发送聊天消息，
         window.parent.postMessage(JSON.stringify(obj),'*');
     };
